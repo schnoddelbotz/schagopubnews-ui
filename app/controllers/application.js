48f98c792fa6e455695ee54dd592768f75f9fc76
Jan Hacker <jan@hacker.ch>
2020-09-05 02:54:38 +0200

    add regular svg, add some more target menu items

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -9,4 +9,15 @@ export default class ApplicationController extends Controller {
     logout() {
         this.session.invalidate();
     }
+
+    @action
+  	toggleSidebar() {
+    	console.log("TOGGLE SIDEBAR!");
+    	// from: js/sb-admin-2.js
+    	// $("body").toggleClass("sidebar-toggled");
+    	// $(".sidebar").toggleClass("toggled");
+    	// if ($(".sidebar").hasClass("toggled")) {
+    	//   $('.sidebar .collapse').collapse('hide');
+    	// };
+  	}
 }
