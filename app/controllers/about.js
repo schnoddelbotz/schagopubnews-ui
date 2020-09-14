30a057a8cc4edab88185db105cf451b1bcc8a5d0
Jan Hacker <jan@hacker.ch>
2020-09-14 22:04:19 +0200

    add ember-tui-editor

---
+++ app/controllers/about.js
@@ -0,0 +1,32 @@
+import Controller from '@ember/controller';
+
+export default class AboutController extends Controller {
+	value = "# Markdown editor demo heading 1\n\nJust to **showcase** *the* tui.editor / ember-tui-editor ...\n\n ## More stuff \n\n here to come.";
+	previewStyle = "tab";
+	editType = "wysiwyg";
+	// https://github.com/evocount/ember-tui-editor
+	// https://nhn.github.io/tui.editor/latest/tutorial-example19-customizing-toolbar-buttons
+	toolbarItems = [
+          'heading',
+          'bold',
+          'italic',
+          'strike',
+          'divider',
+          'hr',
+          'quote',
+          'divider',
+          'ul',
+          'ol',
+          // 'task',
+          'indent',
+          'outdent',
+          'divider',
+          'table',
+          // 'image',
+          // 'link',
+          // 'divider',
+          // 'code',
+          // 'codeblock',
+          'divider',
+    ];
+}
